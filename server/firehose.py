255ddb45d02a66ddc6519f6765b3610b89cecc50
Emily Hunt <emily.hunt.physics@gmail.com>
2023-07-22 19:05:27 +0200

    Hopefully improve reliability of error callback

--- server/firehose.py
+++ server/firehose.py
@@ -98,29 +98,21 @@ def run(stream_stop_event=None):
 
         operations_callback(_get_ops_by_type(commit))
 
-    fails = 0
-    last_fail_time = 0
-
     def on_error_callback(e):
         logger.info(f"Exception encountered in on_message_handler! {e}")
         traceback.print_exc()
 
-        global fails, last_fail_time
-
-        if time.time() - 60 < last_fail_time:
-            fails += 1
-        else:
-            fails = 0
-        last_fail_time = time.time()
-        
-        if fails >= 5:
-            logger.info(f"Encountered more than 5 errors in 60 seconds! Closing firehose...")
-            client.stop()
-            return
-
         logger.info("Trying to re-open database connection (as this is a common issue...)")
-        if db.is_closed():
-            db.connect()
+        try:
+            db.close()
+        except Exception as e:
+            pass
 
+        try:
+            db.connect()
+        except Exception as e:
+            logger.info("Unable to re-open database connection. Stopping client.")
+            traceback.print_exc()
+            client.stop()
 
     client.start(on_message_handler, on_error_callback)
