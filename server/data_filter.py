89b2cbdfc56c9cfe5e7ae3f9f9cc16d68878cc43
emilyhunt <emily.hunt.physics@gmail.com>
2023-11-03 15:29:26 +0100

    Update data filter

--- server/data_filter.py
+++ server/data_filter.py
@@ -63,66 +63,40 @@ post_list = PostList(with_database_closing=True)
 
 
 def operations_callback(ops: dict) -> None:
-    # Here we can filter, process, run ML classification, etc.
-    # After our feed alg we can save posts into our DB
-    # Also, we should process deleted posts to remove them from our DB and keep it in sync
-
-    # for example, let's create our custom feed that will contain all posts that contains alf related text
-
+    # See how many posts are valid
     posts_to_create = []
     valid_dids = account_list.get_accounts()
     valid_posts = [post for post in ops['posts']['created'] if post['author'] in valid_dids]
 
-    astro_feed_counter = 0
+    # Classify valid posts into feed categories
     for created_post in valid_posts:
         post_text = created_post['record']['text']
-        add_to_feed_astro = post_is_valid(post_text)
-        astro_feed_counter += int(add_to_feed_astro)
-
         post_dict = {
             'uri': created_post['uri'],
             'cid': created_post['cid'],
             'author': created_post['author'],
             'text': post_text,
-            'feed_all': True,
-            'feed_astro': add_to_feed_astro,
-            # 'reply_parent': reply_parent,
-            # 'reply_root': reply_root,
         }
         post_dict.update(post_in_feeds(post_text))
         posts_to_create.append(post_dict)
 
+    # See if there are any posts that need deleting
     posts_to_delete = [post['uri'] for post in ops['posts']['deleted'] if post['uri'] in post_list.get_posts()]
 
+    # Perform database operations
     if posts_to_delete or posts_to_create:
-
-        start_time = time.time()
         db.connect(reuse_if_open=True)
-        open_time = time.time()
 
         if posts_to_delete:
             Post.delete().where(Post.uri.in_(posts_to_delete))
             post_list.remove_posts(posts_to_delete)
-            logger.info(f'Deleted from feed: {len(posts_to_delete)}')
-
-        delete_time = time.time()
+            logger.info(f'Deleted posts: {len(posts_to_delete)}')
 
         if posts_to_create:
             with db.atomic():
                 for post_dict in posts_to_create:
                     Post.create(**post_dict)
             post_list.add_posts([x['uri'] for x in posts_to_create])
-            logger.info(f'Added to astro-all: {len(posts_to_create)}; astro: {astro_feed_counter}')
-
-        add_time = time.time()
+            logger.info(f'Added posts: {len(posts_to_create)}')
 
         db.close()
-
-        close_time = time.time()
-
-        close_time -= add_time
-        add_time -= delete_time
-        delete_time -= open_time
-        open_time -= start_time
-
-        logger.info(f'Timing stats:\nopn: {1000 * open_time:.3f}  del: {1000 * delete_time:.3f} add: {1000 * add_time:.3f} clo: {1000 * close_time:.3f}')
