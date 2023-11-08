ed0c80dbe4b8084b8eb48f3fec5c3934d9c11d05
Emily Hunt <emily.hunt.physics@gmail.com>
2023-11-08 22:16:12 +0100

    Update pinned post with handle change warning

--- src/astrofeed-server/pinned.py
+++ src/astrofeed-server/pinned.py
@@ -1,9 +1,10 @@
 import random
 
 PINNED_POSTS = {
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kc632qlmnm2j": 1.0,  # Signup instructions
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdheyu7hct24": 1.0,  # Exoplanet feed announcement
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdhfpnyeff2c": 0.5,  # Like the feed pls
+    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdpeduzh272i": 1.0,  # Don't change your handle
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kc632qlmnm2j": 1.0,  # Signup instructions
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdheyu7hct24": 1.0,  # Exoplanet feed announcement
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdhfpnyeff2c": 0.5,  # Like the feed pls
 }
 
 def add_pinned_post_to_feed(body):
