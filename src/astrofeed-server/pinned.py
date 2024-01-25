3be88ed1a3a0ee03e2583d8de1a1b10e99934c41
emilyhunt <emily.hunt.physics@gmail.com>
2024-01-25 12:32:43 +0100

    update pinned post due to borked feed

--- src/astrofeed-server/pinned.py
+++ src/astrofeed-server/pinned.py
@@ -1,10 +1,11 @@
 import random
 
 PINNED_POSTS = {
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kc632qlmnm2j": 1.5,  # Signup instructions
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdheyu7hct24": 0.5,  # Exoplanet feed announcement
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdtlml2mvf2z": 0.5,  # Astrophotography feed announcement
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdhfpnyeff2c": 0.5,  # Like the feed pls
+    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kjsi4yyhm22t": 1.0,  # Feed is broken
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kc632qlmnm2j": 1.0,  # Signup instructions
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdheyu7hct24": 0.5,  # Exoplanet feed announcement
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdtlml2mvf2z": 0.5,  # Astrophotography feed announcement
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kdhfpnyeff2c": 0.5,  # Like the feed pls
 }
 
 def add_pinned_post_to_feed(body):
