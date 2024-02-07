55826cd945b782d684ed9b10e6a7d56040d640aa
Emily Hunt <emily.hunt.physics@gmail.com>
2024-02-07 15:56:41 +0100

    First version of multi-threaded post classification that's AWS compatible (#6)

--- server/data_filter.py
+++ server/data_filter.py
@@ -1,44 +1,37 @@
 """Logic for how commits are filtered."""
 import logging
 from astrofeed_lib.database import db, Post
-from astrofeed_lib.accounts import AccountList
-from astrofeed_lib.posts import PostList
 from astrofeed_lib.feeds import post_in_feeds
 
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-    
-
-account_list = AccountList(with_database_closing=True)
-account_list.get_accounts()  # Initial get
-    
-
-post_list = PostList(with_database_closing=True)
-post_list.get_posts()  # Initial get
 
 
-def operations_callback(ops: dict, cursor: int) -> None:
+def operations_callback(
+    ops: dict, cursor: int, valid_accounts: set, existing_posts: set
+) -> list:
     # See how many posts are valid
     posts_to_create = []
-    valid_dids = account_list.get_accounts()
-    valid_posts = [post for post in ops['posts']['created'] if post['author'] in valid_dids]
+    valid_posts = [
+        post for post in ops["posts"]["created"] if post["author"] in valid_accounts
+    ]
 
     # Classify valid posts into feed categories
     feed_counts = {}
     for created_post in valid_posts:
         # Don't add a post if it already exists (e.g. if we're looping over the firehose)
-        if created_post['uri'] in post_list.get_posts():
-            logger.info(f'Ignored duplicate post (cursor={cursor})')
+        if created_post["uri"] in existing_posts:
+            logger.info(f"Ignored duplicate post (cursor={cursor})")
             continue
 
         # Basic post info to add to the database
-        post_text = created_post['record']['text']
+        post_text = created_post["record"]["text"]
         post_dict = {
-            'uri': created_post['uri'],
-            'cid': created_post['cid'],
-            'author': created_post['author'],
-            'text': post_text,
+            "uri": created_post["uri"],
+            "cid": created_post["cid"],
+            "author": created_post["author"],
+            "text": post_text,
         }
 
         # Add labels to the post for
@@ -56,7 +49,11 @@ def operations_callback(ops: dict, cursor: int) -> None:
             feed_counts[a_key] += feed_labels[a_key]
 
     # See if there are any posts that need deleting
-    posts_to_delete = [post['uri'] for post in ops['posts']['deleted'] if post['uri'] in post_list.get_posts()]
+    posts_to_delete = [
+        post["uri"]
+        for post in ops["posts"]["deleted"]
+        if post["uri"] in existing_posts
+    ]
 
     # Perform database operations
     if posts_to_delete or posts_to_create:
@@ -64,15 +61,21 @@ def operations_callback(ops: dict, cursor: int) -> None:
 
         if posts_to_delete:
             Post.delete().where(Post.uri.in_(posts_to_delete))
-            post_list.remove_posts(posts_to_delete)
-            logger.info(f'Deleted posts: {len(posts_to_delete)} (cursor={cursor})')
+            # existing_posts.remove(posts_to_delete)
+            logger.info(f"Deleted posts: {len(posts_to_delete)} (cursor={cursor})")
 
         if posts_to_create:
             with db.atomic():
                 for post_dict in posts_to_create:
                     Post.create(**post_dict)
-            post_list.add_posts([x['uri'] for x in posts_to_create])
-            feed_counts_string = ", ".join([f"{key[5:]}-{feed_counts[key]}" for key in feed_counts])
-            logger.info(f'Added posts: {feed_counts_string} (cursor={cursor})')
+            # existing_posts.add([x["uri"] for x in posts_to_create])
+            feed_counts_string = ", ".join(
+                [f"{key[5:]}-{feed_counts[key]}" for key in feed_counts]
+            )
+            logger.info(f"Added posts: {feed_counts_string} (cursor={cursor})")
 
         db.close()
+
+    if not posts_to_create:
+        return []
+    return [post["uri"] for post in posts_to_create]
