bce7af9e41b5476ff4feed6e9e0dd851d1fa3775
Emily Hunt <emily.hunt.physics@gmail.com>
2024-02-07 11:28:31 +0100

    Account for moved PostList

--- server/data_filter.py
+++ server/data_filter.py
@@ -2,9 +2,8 @@
 import logging
 from astrofeed_lib.database import db, Post
 from astrofeed_lib.accounts import AccountList
+from astrofeed_lib.posts import PostList
 from astrofeed_lib.feeds import post_in_feeds
-import time
-from datetime import datetime, timedelta
 
 
 logger = logging.getLogger(__name__)
@@ -13,52 +12,6 @@ logging.basicConfig(level=logging.INFO)
 
 account_list = AccountList(with_database_closing=True)
 account_list.get_accounts()  # Initial get
-
-
-class PostList:
-    def __init__(
-            self, 
-            with_database_closing: bool=False, 
-            query_interval: int=60*60*24, 
-            max_post_age: timedelta=timedelta(days=7)
-        ) -> None:
-        """Generic refreshing post list. Tries to reduce number of required query operations!"""
-        self.last_query_time = time.time()
-        self.query_interval = query_interval
-        self.posts = set()
-        self.max_post_age = max_post_age
-        if with_database_closing:
-            self.query_database = self.query_database_with_closing
-        else:
-            self.query_database = self.query_database_without_closing
-
-    def query_database_without_closing(self) -> None:
-        db.connect(reuse_if_open=True)
-        self.posts = self.post_query()
-
-    def query_database_with_closing(self) -> None:
-        db.connect(reuse_if_open=True)
-        self.posts = self.post_query()
-        db.close()
-
-    def post_query(self):
-        """Intended to be overwritten! Should return a set of posts."""
-        return {post.uri for post in Post.select().where(Post.indexed_at > datetime.now() - self.max_post_age)}
-
-    def get_posts(self) -> set:
-        is_overdue = time.time() - self.last_query_time > self.query_interval
-        if is_overdue or len(self.posts) == 0:
-            self.query_database()
-            self.last_query_time = time.time()
-        return self.posts
-    
-    def add_posts(self, posts):
-        for post in posts:
-            self.posts.add(post)
-
-    def remove_posts(self, posts):
-        for post in posts:
-            self.posts.remove(post)
     
 
 post_list = PostList(with_database_closing=True)
