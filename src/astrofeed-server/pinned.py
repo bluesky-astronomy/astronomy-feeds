9be5c01fcfb9747d50271307b2902a005bed000d
emilyhunt <emily.hunt.physics@gmail.com>
2024-04-08 09:00:35 +0200

    Add eclipse PSA pinned post

--- src/astrofeed-server/pinned.py
+++ src/astrofeed-server/pinned.py
@@ -1,10 +1,11 @@
 import random
 
 PINNED_POSTS = {
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kc632qlmnm2j": 2.5,  # Signup instructions
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkrhwzmhg22i": 1.5,  # 'Bluesky is open now'
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkri5olz3526": 1.5,  # List of all feeds
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkrivv43ve2z": 0.5,  # Like the feed pls
+    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kpm3fen57v2c": 1.0,  # Eclipse PSA
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kc632qlmnm2j": 2.5,  # Signup instructions
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkrhwzmhg22i": 1.5,  # 'Bluesky is open now'
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkri5olz3526": 1.5,  # List of all feeds
+    # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkrivv43ve2z": 0.5,  # Like the feed pls
     # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkris4cvel2o": 1.5,  # Getting started guide
 }
 
