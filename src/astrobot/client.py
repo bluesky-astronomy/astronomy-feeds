82171982356e996715caaa6e70749780df538795
Emily Hunt <emily.hunt.physics@gmail.com>
2024-06-16 20:28:06 +0200

    Rewrite of notification system

---
+++ src/astrobot/client.py
@@ -0,0 +1,77 @@
+"""Standard functions for working with clients. Mostly just wraps atproto."""
+from atproto import Client, Session, SessionEvent
+import os
+
+
+def get_client(
+    handle_env_var: str, password_env_var: str, reuse_session: bool = True
+) -> Client:
+    """A standard function for getting a valid client - already logged in and 
+    ready to go =)
+    """       
+    # Set up client and set it up to save its session incrementally
+    client = Client()
+    handle = _get_handle(handle_env_var)
+    session_updater = BotSessionUpdater(handle)
+    client.on_session_change(session_updater.on_session_change)
+
+    # Login using previous session
+    session = _get_session(handle)
+    if session:
+        print("Reusing existing session")
+        try:
+            client.login(session_string=session)
+            return client
+        except Exception as e:
+            print(f"Unable to log in with previous session! Reason: {e}")
+
+    # We revert to password login if we can't find a session or if there was an issue
+    print("Logging in with password instead...")
+    password = _get_password(password_env_var)
+    client.login(handle, password)
+    return client
+
+
+def _get_session(handle: str) -> str | None:
+    try:
+        with open(f'{handle}.session') as f:
+            return f.read()
+    except FileNotFoundError:
+        return None
+
+
+def _get_handle(handle_env_var: str):
+    """Checks and/or gets a handle for the client from an environment variable."""
+    handle = os.getenv(handle_env_var, None)
+    if handle is None:
+        raise ValueError(
+            f"You need to set the environment variable {handle_env_var} to your handle."
+        )
+    return handle
+
+
+def _get_password(password_env_var: str):
+    """Checks and/or gets a password for the client from an environment variable."""
+    password = os.getenv(password_env_var, None)
+    if password is None:
+        raise ValueError(
+            f"You need to set the environment variable {password_env_var} to your Bluesky app password."
+        )
+    return password
+
+
+class BotSessionUpdater:
+    def __init__(self, handle):
+        """Simple class to save a bot's session to a file named {handle}.session."""
+        self.handle = handle
+
+    def on_session_change(self, event: SessionEvent, session: Session) -> None:
+        """Callback to save session."""
+        print('Session changed:', event, repr(session))
+        if event in (SessionEvent.CREATE, SessionEvent.REFRESH):
+            print('Saving changed session')
+            self.save_session(session.export())
+
+    def save_session(self, session_string: str) -> None:
+        with open(f'{self.handle}.session', 'w') as f:
+            f.write(session_string)
---
+++ src/astrobot/notifications.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from atproto import Client
+
+
+ALLOWED_NOTIFICATION_TYPES = {"like", "repost", "follow", "mention", "reply", "quote"}
+
+
+def get_notifications(
+    client: Client,
+    types: None | set[str] = None,
+    limit: int | None = 50,
+    seen_at: str | None = None,
+    fetch_all: bool = True,
+    unread_only: bool = True,
+) -> None:
+    """Gets all current notifications attached to a client."""
+    current_time = client.get_current_time_iso()
+
+    # Fetch all notifications
+    responses, last_seen_time = _fetch_notifications_recursive(
+        client, limit, seen_at, fetch_all
+    )
+
+    all_notifications = []
+    for response in responses:
+        all_notifications.extend(response.notifications)
+
+    # Filter notifications to only desired ones
+    if types:
+        all_notifications = [n for n in all_notifications if n.reason in types]
+
+    if unread_only:
+        all_notifications = [
+            n
+            for n in all_notifications
+            if iso_time_to_datetime(n.indexed_at) > last_seen_time
+        ]
+
+    return all_notifications, current_time
+
+
+def _fetch_notifications_recursive(
+    client: Client, limit: int = 50, seen_at: None | str = None, fetch_all: bool = True
+):
+    responses = []
+    cursor = None
+    while True:
+        responses.append(
+            client.app.bsky.notification.list_notifications(
+                params=dict(limit=limit, seen_at=seen_at, cursor=cursor)
+            )
+        )
+        last_seen_time = iso_time_to_datetime(responses[-1].seen_at)
+
+        if not fetch_all or responses[-1].cursor is None:
+            return responses, last_seen_time
+
+        cursor = responses[-1].cursor
+        if iso_time_to_datetime(cursor) < last_seen_time:
+            return responses, last_seen_time
+
+
+def update_last_seen_time(client: Client, current_time: str):
+    client.app.bsky.notification.update_seen({"seen_at": current_time})
+
+
+def datetime_to_iso_time(date: datetime):
+    return date.isoformat()
+
+
+def iso_time_to_datetime(date: str):
+    return datetime.fromisoformat(date)
