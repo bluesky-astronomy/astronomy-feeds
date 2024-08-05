1fafc87c628f73eaa1230b44d09c9a449e6e9936
Emily Hunt <emily.hunt.physics@gmail.com>
2024-08-05 12:25:01 +0200

    Update pinned posts

--- src/astrofeed-server/pinned.py
+++ src/astrofeed-server/pinned.py
@@ -1,11 +1,19 @@
 import random
 
+# DIDs
+# emily.space: did:plc:jcoy7v3a2t4rcfdh6i4kza25
+# astronomy.blue: did:plc:xy2zorw2ys47poflotxthlzg
+
+
+# Pinned posts dict, where:
+# - keys are a link to the post in ATProto format
+# - values are the post weight (higher = more likely to be shown)
 PINNED_POSTS = {    
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kc632qlmnm2j": 2.5,  # Signup instructions
+    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxnbxxp542t": 2.5,  # Signup instructions
     # # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkrhwzmhg22i": 1.5,  # 'Bluesky is open now'
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkri5olz3526": 1.5,  # List of all feeds
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkrivv43ve2z": 0.5,  # Like the feed pls
-    "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkris4cvel2o": 1.5,  # Getting started guide
+    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxng2wims2n": 1.5,  # List of all feeds
+    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxnyimjna2a": 0.5,  # Like the feed pls
+    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxobfx76e2n": 1.5,  # Getting started guide
 }
 
 def add_pinned_post_to_feed(body):
