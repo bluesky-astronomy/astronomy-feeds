c6599a4812e7c76f2067cd6ba5e68d402f7036f9
Emily Hunt <emily.hunt.physics@gmail.com>
2024-08-05 20:57:21 +0200

    Add better permissions support to commands

--- src/astrobot/commands/_base.py
+++ src/astrobot/commands/_base.py
@@ -4,17 +4,18 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from atproto import Client
 from ..notifications import LikeNotification, ReplyNotification, MentionNotification
+from ..post import send_post
+from astrobot.moderation import MODERATORS
+from astrobot.database import new_bot_action
 
 
 class Command(ABC):
     command = ""  # should be set by subclasses
-    level = "user"  # user or moderator; user by default
+    level = 0  # user or moderator level; user (0) by default.
 
     @staticmethod
     @abstractmethod
-    def is_instance_of(
-        notification: MentionNotification
-    ) -> None | Command:
+    def is_instance_of(notification: MentionNotification) -> None | Command:
         """Check if a given string is a valid example of this command.
 
         If yes, then return an intialized version of this class with the command
@@ -24,9 +25,41 @@ class Command(ABC):
         """
         pass
 
-    @abstractmethod
     def execute(self, client: Client):
         """Executes the next step of a given command, or update to a command."""
+        if (reason := self.user_cannot_use_command()):
+            self.execute_no_permissions(self, client, reason)
+        self.execute_good_permissions
+
+    def user_cannot_use_command(
+        self
+    ):
+        if self.level == 0:
+            return False
+        if self.notification.author.did in MODERATORS.get_accounts_above_level(self.level):
+            return False
+        # Todo: block banned/muted users from ever touching a command on the bot
+        # Todo: make it so a banned user, if trying to use a command, actually gets banned on a Bluesky level so that they physically can't interact with it
+        return f"Lacking required moderator level ({self.level})"
+
+    def execute_no_permissions(self, client: Client, reason: str):
+        """Tells the user that they did not have the required permissions to run this
+        command.
+        """
+        send_post(
+            client,
+            "Sorry, but you don't have the required permissions to run this command. "
+            f"Reason: {reason}",
+            root_post=self.notification.root_ref,
+            parent_post=self.notification.parent_ref,
+        )
+
+        # Record this attempt to use a mod command
+        new_bot_action(self, authorized=False)
+
+
+    @abstractmethod
+    def execute_good_permissions(self, client: Client):
         pass
 
 
