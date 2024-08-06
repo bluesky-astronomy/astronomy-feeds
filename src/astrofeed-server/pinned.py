e7390dd53e44eaf7ced23066dace0784634efaf8
Emily Hunt <emily.hunt.physics@gmail.com>
2024-08-06 10:29:06 +0200

    Update pinned post to point to bot

--- src/astrofeed-server/pinned.py
+++ src/astrofeed-server/pinned.py
@@ -9,7 +9,7 @@ import random
 # - keys are a link to the post in ATProto format
 # - values are the post weight (higher = more likely to be shown)
 PINNED_POSTS = {    
-    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxnbxxp542t": 2.5,  # Signup instructions
+    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyzye4lujs2w": 2.5,  # Signup instructions
     # # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkrhwzmhg22i": 1.5,  # 'Bluesky is open now'
     "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxng2wims2n": 1.5,  # List of all feeds
     "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxnyimjna2a": 0.5,  # Like the feed pls
