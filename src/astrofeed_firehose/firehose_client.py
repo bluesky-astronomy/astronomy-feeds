3665f79ed7c0f18828c520d03b4129999115933b
Emily Hunt <emily.hunt.physics@gmail.com>
2024-09-01 18:06:12 +0200

    Make client asynchronous

--- src/astrofeed_firehose/firehose_client.py
+++ src/astrofeed_firehose/firehose_client.py
@@ -1,52 +1,38 @@
 """Code for client that connects to firehose."""
+
 import logging
 from multiprocessing.sharedctypes import Synchronized
 from multiprocessing.connection import Connection
 import time
 from atproto.exceptions import FirehoseError
-from atproto import FirehoseSubscribeReposClient
+from atproto import AsyncFirehoseSubscribeReposClient
 from atproto import firehose_models
 from atproto import models
 from atproto_client.models.common import XrpcError
 from astrofeed_lib.config import SERVICE_DID
 from astrofeed_lib.database import SubscriptionState
+import asyncio
 
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def _get_start_cursor():
-    # Get current saved cursor value
-    start_cursor = SubscriptionState.get(
-        SubscriptionState.service == SERVICE_DID
-    ).cursor
-    if start_cursor:
-        if not isinstance(start_cursor, int):
-            raise ValueError(f"Saved cursor with value '{start_cursor}' is invalid.")
-        return start_cursor
-
-    # If there isn't one, then make sure the DB has a cursor
-    logger.info("Generating a cursor for the first time...")
-    SubscriptionState.create(service=SERVICE_DID, cursor=0)
-    return None
-
-
-def _get_client(
-    start_cursor: int | None = None, base_uri: str = "wss://bsky.network/xrpc"
-):
-    if start_cursor is None:
-        start_cursor = _get_start_cursor()
-    params = models.ComAtprotoSyncSubscribeRepos.Params(cursor=start_cursor)
-    return FirehoseSubscribeReposClient(params, base_uri=base_uri)
-
-
-def _is_client_too_slow_error(e):
-    xrpc_error = e.args[0]
-    return isinstance(xrpc_error, XrpcError) and xrpc_error.error == "ConsumerTooSlow"
-
-
 def run_client(
+    cursor: Synchronized,  # Return value of multiprocessing.Value
+    pipe: Connection,
+    firehose_time: Synchronized,  # Return value of multiprocessing.Value
+    start_cursor: int | None = None,
+    base_uri: str = "wss://bsky.network/xrpc",
+):
+    asyncio.run(
+        run_client_async(
+            cursor, pipe, firehose_time, start_cursor=start_cursor, base_uri=base_uri
+        )
+    )
+
+
+async def run_client_async(
     cursor: Synchronized,  # Return value of multiprocessing.Value
     pipe: Connection,
     firehose_time: Synchronized,  # Return value of multiprocessing.Value
@@ -57,7 +43,7 @@ def run_client(
     immediately sent to the separate post processing worker.
     """
 
-    def on_message_handler(message: firehose_models.MessageFrame) -> None:
+    async def on_message_handler(message: firehose_models.MessageFrame) -> None:
         """This handler tells the client what to do when a new commit is encountered."""
         # Send it to post processing worker to handle
         pipe.send(message)
@@ -79,9 +65,39 @@ def run_client(
 
         try:
             logger.info("... firehose client worker started")
-            client.start(on_message_handler)
+            await client.start(on_message_handler)
 
         except FirehoseError as e:
             if not _is_client_too_slow_error(e):
                 raise e
             logger.warn("Reconnecting to Firehose due to ConsumerTooSlow...")
+
+
+def _get_client(
+    start_cursor: int | None = None, base_uri: str = "wss://bsky.network/xrpc"
+):
+    if start_cursor is None:
+        start_cursor = _get_start_cursor()
+    params = models.ComAtprotoSyncSubscribeRepos.Params(cursor=start_cursor)
+    return AsyncFirehoseSubscribeReposClient(params, base_uri=base_uri)
+
+
+def _get_start_cursor():
+    # Get current saved cursor value
+    start_cursor = SubscriptionState.get(
+        SubscriptionState.service == SERVICE_DID
+    ).cursor
+    if start_cursor:
+        if not isinstance(start_cursor, int):
+            raise ValueError(f"Saved cursor with value '{start_cursor}' is invalid.")
+        return start_cursor
+
+    # If there isn't one, then make sure the DB has a cursor
+    logger.info("Generating a cursor for the first time...")
+    SubscriptionState.create(service=SERVICE_DID, cursor=0)
+    return None
+
+
+def _is_client_too_slow_error(e):
+    xrpc_error = e.args[0]
+    return isinstance(xrpc_error, XrpcError) and xrpc_error.error == "ConsumerTooSlow"
