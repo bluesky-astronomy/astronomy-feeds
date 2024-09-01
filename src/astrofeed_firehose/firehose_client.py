366b752764fc305dad4eca34fa0c747d4d30b4a5
Emily Hunt <emily.hunt.physics@gmail.com>
2024-09-01 15:21:10 +0200

    Small bit of benchmarking

--- src/astrofeed_firehose/firehose_client.py
+++ src/astrofeed_firehose/firehose_client.py
@@ -3,6 +3,7 @@ import logging
 from multiprocessing.sharedctypes import Synchronized
 from multiprocessing.connection import Connection
 import time
+from unittest import findTestCases
 from atproto.exceptions import FirehoseError
 from atproto import FirehoseSubscribeReposClient
 from atproto import firehose_models
@@ -10,6 +11,7 @@ from atproto import models
 from atproto_client.models.common import XrpcError
 from astrofeed_lib.config import SERVICE_DID
 from astrofeed_lib.database import SubscriptionState
+import comm
 
 
 logger = logging.getLogger(__name__)
@@ -46,6 +48,11 @@ def _is_client_too_slow_error(e):
     return isinstance(xrpc_error, XrpcError) and xrpc_error.error == "ConsumerTooSlow"
 
 
+firehose_start = time.time()
+on_message_handler_total = 0.
+commits = 0
+
+
 def run_client(
     cursor: Synchronized,  # Return value of multiprocessing.Value
     pipe: Connection,
@@ -56,11 +63,16 @@ def run_client(
     """Primary function for running the client that connects to Bluesky. New commits are
     immediately sent to the separate post processing worker.
     """
+    
 
     def on_message_handler(message: firehose_models.MessageFrame) -> None:
         """This handler tells the client what to do when a new commit is encountered."""
+        global firehose_start, on_message_handler_total, commits
+
         # Send it to post processing worker to handle
+        on_message_handler_start = time.time()
         pipe.send(message)
+        on_message_handler_finish = time.time()
 
         # Update local client cursor value
         if cursor.value:
@@ -71,6 +83,19 @@ def run_client(
 
         # Update current working time so that the watchdog knows this process is running
         firehose_time.value = time.time()
+
+        # TODO: remove benchmarking
+        
+        
+        on_message_handler_total = on_message_handler_total + on_message_handler_finish - on_message_handler_start
+        commits = commits + 1
+        if commits % 10000 == 0:
+            firehose_total = time.time() - firehose_start
+            print(on_message_handler_total / firehose_total, firehose_total, on_message_handler_total)
+
+            firehose_start = time.time()
+            on_message_handler_total = 0.
+        
 
     # Continually restarts the client if ConsumerTooSlow errors are encountered. This
     # can happen due to the Bluesky network being busy or internet connection issues.
