f4bb20b2f789d21fd1d5c74c782c28c6ff3e0063
emilyhunt <emily.hunt.physics@gmail.com>
2024-09-12 17:28:55 +0100

    Update pinned post to mention today's new feeds

--- src/astrofeed_server/pinned.py
+++ src/astrofeed_server/pinned.py
@@ -12,7 +12,8 @@ PINNED_POSTS = {
     "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyzye4lujs2w": 3.0,  # Signup instructions
     # "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kzlu464pok2t": 3.0,  # Moderator applications
     # # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkrhwzmhg22i": 1.5,  # 'Bluesky is open now'
-    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxng2wims2n": 1.5,  # List of all feeds
+    # "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxng2wims2n": 1.5,  # List of all feeds
+    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3l3xtpvv46k24": 1.5,  # List of new feeds from Sep 12th 2024
     "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxnyimjna2a": 0.5,  # Like the feed pls
     "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxobfx76e2n": 1.5,  # Getting started guide
 }
