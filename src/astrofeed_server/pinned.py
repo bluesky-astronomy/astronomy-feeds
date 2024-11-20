04408fc44192a85c848d35ca2066c0d03a2a055d
emilyhunt <emily.hunt.physics@gmail.com>
2024-11-20 11:11:08 +0100

    Add mod applications to pinned post

--- src/astrofeed_server/pinned.py
+++ src/astrofeed_server/pinned.py
@@ -10,12 +10,11 @@ import random
 # - values are the post weight (higher = more likely to be shown)
 PINNED_POSTS = {    
     "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyzye4lujs2w": 3.0,  # Signup instructions
-    # "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kzlu464pok2t": 3.0,  # Moderator applications
+    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3lbepgrmp3s2c": 1.5,  # Moderator applications 2.0
     # # "at://did:plc:jcoy7v3a2t4rcfdh6i4kza25/app.bsky.feed.post/3kkrhwzmhg22i": 1.5,  # 'Bluesky is open now'
-    # "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxng2wims2n": 1.5,  # List of all feeds
-    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3lau3lw3mk22b": 1.5,  # List of all feeds from November 13th 2024
+    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3l3xtpvv46k24": 1.5,  # List of new feeds from Sep 12th 2024
     "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxnyimjna2a": 0.5,  # Like the feed pls
-    "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxobfx76e2n": 1.5,  # Getting started guide
+    # "at://did:plc:xy2zorw2ys47poflotxthlzg/app.bsky.feed.post/3kyxobfx76e2n": 1.5,  # Getting started guide
 }
 
 def add_pinned_post_to_feed(body):
