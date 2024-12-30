6e4a3f3f3a0db96012219cc8099cf6ff84fb83aa
Emily Hunt <emily.hunt.physics@gmail.com>
2024-12-30 18:05:21 +0100

    v1 of hiding command

--- src/astrobot/commands/moderation/hide.py
+++ src/astrobot/commands/moderation/hide.py
@@ -5,6 +5,9 @@ from __future__ import annotations
 from atproto import Client
 from astrobot.commands._base import Command
 from astrobot.notifications import MentionNotification
+from astrobot.database import new_bot_action
+from astrobot.moderation import hide_post
+from astrobot.post import send_post
 
 
 class ModeratorHideCommand(Command):
@@ -12,7 +15,6 @@ class ModeratorHideCommand(Command):
     level = 2
 
     def __init__(self, notification: MentionNotification):
-        raise NotImplementedError()
         self.notification = notification
 
     @staticmethod
@@ -23,4 +25,22 @@ class ModeratorHideCommand(Command):
             return ModeratorHideCommand(notification)
 
     def execute_good_permissions(self, client: Client):
-        pass  # Todo
+        # Default failure case
+        explanation = "Unable to hide post: this command must be in reply to the post to hide."
+        
+        # Check that this post is a reply to something
+        if hasattr(self.notification.record, "reply"):
+            # Attempt to hide the post it's replied to
+            uri_to_hide = self.notification.record.reply.parent.uri
+            author_did = uri_to_hide.replace("at://", "").split("/")[0]
+            mod_did = self.notification.author.did
+            explanation = hide_post(uri_to_hide, author_did, mod_did)
+
+        # & inform the user
+        send_post(
+            client,
+            explanation,
+            root_post=self.notification.root_ref,
+            parent_post=self.notification.parent_ref,
+        )
+        new_bot_action(self)
