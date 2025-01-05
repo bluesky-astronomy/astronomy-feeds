8d07bf20fc6d177016285c0e2333437390e1686a
Emily Hunt <emily.hunt.physics@gmail.com>
2025-01-05 10:57:57 +0100

    Add logging config

--- src/astrofeed_firehose/__main__.py
+++ src/astrofeed_firehose/__main__.py
@@ -3,21 +3,30 @@ firehose client and post processor on separate subprocesses.
 """
 
 import time
+import logging
 from astrofeed_firehose.manager import FirehoseProcessingManager
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s %(levelname)s] %(message)s",
+    datefmt="%y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+
 if __name__ == "__main__":
-    print("Initializing firehose processing manager!")
+    logger.info("Initializing firehose processing manager!")
     manager = FirehoseProcessingManager()
 
-    print("Starting child subprocesses")
+    logger.info("Starting child subprocesses")
     manager.start_processes()
 
     try:
-        print("Starting continuous monitoring of processes")
+        logger.info("Starting continuous monitoring of processes")
         time.sleep(5)  # Give it a sec to start up
         manager.monitor()
 
     except KeyboardInterrupt:
-        print("Keyboard interrupt - stopping processes.")
+        logger.info("Keyboard interrupt - stopping processes.")
         manager.stop_processes()
