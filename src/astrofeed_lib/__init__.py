b4244ed8a9d731d65b54c358bc28c6b2e66bba24
Emily Hunt <emily.hunt.physics@gmail.com>
2025-02-02 16:47:49 +0100

    Change default level to INFO - logs are nice!

--- src/astrofeed_lib/__init__.py
+++ src/astrofeed_lib/__init__.py
@@ -3,5 +3,5 @@ from .config import ASTROFEED_PRODUCTION, DEBUG_ENABLED
 
 # set up logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG if ASTROFEED_PRODUCTION is False or DEBUG_ENABLED is True else logging.WARN) # OR check if a debug flag is set for production
+logger.setLevel(logging.DEBUG if ASTROFEED_PRODUCTION is False or DEBUG_ENABLED is True else logging.INFO) # OR check if a debug flag is set for production
 logging.basicConfig(format='%(asctime)s %(name)s - [%(levelname)s] : %(message)s')
