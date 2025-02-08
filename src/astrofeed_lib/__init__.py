098d669707b53c28234842849ea31ec3b4107da0
Emily Hunt <emily.hunt.physics@gmail.com>
2025-02-08 10:59:05 +0100

    A tiny bit more formatting tweaking

--- src/astrofeed_lib/__init__.py
+++ src/astrofeed_lib/__init__.py
@@ -11,9 +11,9 @@ if DEBUG_ENABLED or ASTROFEED_PRODUCTION is False:
 
 # Set logging format. 
 # In production, the logging system captures timestamps, service name & logging level, so these aren't needed in the log.
-_logging_format = '%(asctime)s %(name)s - [%(levelname)s] : %(message)s'
+_logging_format = '%(asctime)s %(name)s [%(levelname)s] %(message)s'
 if ASTROFEED_PRODUCTION:
-    _logging_format = '[%(levelname)s] : %(message)s'
+    _logging_format = '[%(levelname)s] %(message)s'
 
 logger.setLevel(_logging_level)
 logging.basicConfig(format=_logging_format)
