e443901c3f1bbe9bda2c3e5d539c47d81a3399fe
Vincent Carpenter <vincent.s.carpenter@gmail.com>
2025-02-20 13:40:55 -0800

    switched from using dev database to temporary mock database

--- tests/astrobot/test_commands.py
+++ tests/astrobot/test_commands.py
@@ -1,14 +1,35 @@
+import os
 from datetime import datetime
+
+from peewee import DatabaseProxy, SqliteDatabase
 from atproto import Client, models
+
+import astrofeed_lib.database # need to import this as a whole module to mock the database
+from astrofeed_lib.database import BotActions, DBConnection
+from astrofeed_lib.config import ASTROFEED_PRODUCTION
 
 from astrobot.commands.joke import JokeCommand, jokes
 from astrobot.notifications import MentionNotification
 from astrobot.generate_notification import build_notification, build_reply_ref, construct_strong_ref_main
 from astrobot.config import HANDLE
 
-from astrofeed_lib.config import ASTROFEED_PRODUCTION
-from astrofeed_lib.database import BotActions, DBConnection
+# mock database infrastructure
+mock_db = None
+def get_mock_database() -> DatabaseProxy:
+    '''replacement for actual database access'''
+    # if we don't already have a database, create one
+    global mock_db
+    if(mock_db is None):
+        # throwaway SQLite database
+        db = SqliteDatabase("UNIT_TEST_MOCK_DB.db", autoconnect=False)
 
+        # for consistency --- the rest of the code uses initialized proxies
+        mock_db = DatabaseProxy()
+        mock_db.initialize(db)
+
+    return mock_db
+
+# mock client class to replace the network-connected methods
 class MockClient(Client):
     '''Replaces certain atproto.Client methods with offline-test appropriate alternatives'''
     def __init__(self, base_url = None, *args, **kwargs):
@@ -34,24 +55,28 @@ class MockClient(Client):
 
 
 def test_joke_unit():
-    # until I can redirect database access, make sure we're not sending anything to live database
+    # just to be safe, make sure there's no risk of connecting to the live database
     if ASTROFEED_PRODUCTION: raise ConnectionRefusedError("Attempting to run offline unit test in production mode; aborting.")
 
     # create a joke command object with a mock notification
     joke_notification = build_notification("mention", record_text=f"@{HANDLE} joke", author_did="test_joke_unit")
     joke_command = JokeCommand(MentionNotification(joke_notification))
 
+    # replace the database used by astrofeed lib with a mock database
+    astrofeed_lib.database.get_database = get_mock_database
+    astrofeed_lib.database.BotActions._meta.database = astrofeed_lib.database.get_database()
+    with DBConnection() as conn:
+        conn.create_tables([BotActions])
+
     # execute the command with a mock client
     mock_client = MockClient()
     joke_command.execute(mock_client)
 
-    # extract send_post arguments
+    # extract quantities of interest and remove temporary files
     send_post_call_signature = mock_client.send_post_call_signature
-
-    # extract database entry, and remove from database
-    with BotActions._meta.database as DBConnection:
+    with DBConnection():
         test_entry = BotActions.select().where(BotActions.did == "test_joke_unit")[0]
-        test_entry.delete_instance()
+    os.remove("UNIT_TEST_MOCK_DB.db")
 
     # check that send_post values and database entry are correct
     joke_strong_ref = models.create_strong_ref(joke_notification)
