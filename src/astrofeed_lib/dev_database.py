c57ea3ed46fd858b39b876715817b308e7a24d54
Vincent Carpenter <vincent.s.carpenter@gmail.com>
2025-03-06 20:18:03 -0800

    adding sqlite source ingestion to dev_db generation

--- src/astrofeed_lib/dev_database.py
+++ src/astrofeed_lib/dev_database.py
@@ -30,7 +30,7 @@ class DB():
     """
 
     # what is currently supported
-    supported_source_formats      = ["parquet"]
+    supported_source_formats      = ["parquet", "sqlite"]
     supported_sampling_strategies = ["first", "last", "random"]
 
     def __init__(self, db_conn: peewee.Database):
@@ -62,6 +62,12 @@ class DB():
         """
 
         format = format.lower()
+
+        if(format not in self.supported_source_formats):
+            raise NotImplementedError(f"dev_database/DB/populate_from_source: \
+                                        {format} not a supported format, please use one of the following: \
+                                        {self.supported_source_formats}")
+
         match format:
             case "parquet":
                 # assume we are given a directory of tablename.parquet files that match our database structure
@@ -76,6 +82,37 @@ class DB():
                                                       Source directory must contain files for each of these tables: ({self.models.keys()})")
                 else:
                     raise NotADirectoryError(f"dev_database/DB/populate_from_source: No such directory '{source}': expecting a directory.")
+
+            case "sqlite":
+                # assume we're given a pre-existing database that matches the schema defined in our model classes
+
+                # first, make sure we're given a pre-existing database file
+                if not os.path.isfile(source):
+                    raise FileNotFoundError(f"dev_database/DB/populate_from_source: \
+                                            Unable to find {source} file; must be a pre-existing SQLite database file.")
+                try:
+                    source_db_conn = peewee.SqliteDatabase(source)
+                    source_tables = source_db_conn.get_tables()
+                except peewee.DatabaseError as err:
+                    print(f"dev_database/DB/populate_from_source: \
+                          could not parse {source} as an SqLite database file.")
+                    raise err
+
+                # now check to make sure that the database is complete
+                if set(source_tables) != set([table_name.lower() for table_name in self.models.keys()]):
+                    raise RuntimeError(f"dev_database/DB/populate_from_source: \n\
+                                            Database at {source} does not contain all necessary tables to populate a \n\
+                                            new database (has {source_tables} tables, must have {self.models.keys()} tables).")
+
+                # now we can try to transfer database data to our internal representation
+                for model_name, model in self.models.items():
+                    try:
+                        with model.bind_ctx(source_db_conn):
+                            self.data[model_name] = pandas.DataFrame(model.select().dicts())
+                    except peewee.DatabaseError as err:
+                        print(f"dev_database/DB/populate_from_source: \
+                            {model} table from {source} database does not match defined schema.")
+                        raise err
 
             case _:
                 raise NotImplementedError(f"dev_database/DB/populate_from_source: \
@@ -157,26 +194,33 @@ class DB():
                 # different replacement needs for different columns
                 match column:
                     case "indexed_at":
-                        replacement_label = "indexed_at"
+                        if(type(df["indexed_at"].iloc[0]) is datetime):
+                            # no need to clean
+                            continue
+                        else:
+                            replacement_label = "indexed_at"
 
-                        # make copy of column in python date-time format
-                        with warnings.catch_warnings(): # suppresses deprecation warnings from to_pydatetime
-                            warnings.simplefilter("ignore")
-                            replacement_data = df["indexed_at"].dt.to_pydatetime()
-                        replacement_data = pandas.Series(replacement_data, dtype=object)
+                            # make copy of column in python date-time format
+                            with warnings.catch_warnings(): # suppresses deprecation warnings from to_pydatetime
+                                warnings.simplefilter("ignore")
+                                replacement_data = df["indexed_at"].dt.to_pydatetime()
+                            replacement_data = pandas.Series(replacement_data, dtype=object)
 
                     case "checked_at":
-                        replacement_label = "checked_at"
+                        if(type(df["checked_at"].iloc[0]) is datetime):
+                            # no need to clean
+                            continue
+                        else:
+                            # replace possible all-zeroes entries that we can't convert first
+                            date_strings = numpy.array(df["checked_at"])
+                            replacement_label = "checked_at"
+                            replacement_data = []
+                            for i in range(len(date_strings)):
+                                if date_strings[i] == "0000-00-00 00:00:00":
+                                    date_strings[i] = "0001-01-01 00:00:01"
+                                replacement_data.append(datetime.strptime(date_strings[i], "%Y-%m-%d %H:%M:%S"))
 
-                        # replace possible all-zeroes entries that we can't convert first
-                        date_strings = numpy.array(df["checked_at"])
-                        replacement_data = []
-                        for i in range(len(date_strings)):
-                            if date_strings[i] == "0000-00-00 00:00:00":
-                                date_strings[i] = "0001-01-01 00:00:01"
-                            replacement_data.append(datetime.strptime(date_strings[i], "%Y-%m-%d %H:%M:%S"))
-
-                        replacement_data = pandas.Series(replacement_data)
+                            replacement_data = pandas.Series(replacement_data)
 
                     case _: # no cleaning needed
                         continue
@@ -224,17 +268,18 @@ class DB():
 ##
 
 def build_dev_db(data_source: str, 
-                 data_format: str = "parquet", 
+                 source_format: str = "parquet", 
                  database_engine: str = "sqlite", 
                  truncation_num: int = 50000,
                  truncation_frac: float = 0.0,
                  truncation_strat: str = "last",
+                 database_name : str = "dev_db.db",
                  overwrite_existing: bool = False):
     """Build a smaller, developer-friendly database from source data.
 
     in:
         data_souce: string locating un-truncated data to read in
-        data_format: string indicating format of data in source
+        source_format: string indicating format of data in source
         database_engine: string indicating which engine to use for the developer database
         truncation_num: number of entries in the Post table to truncate to
         truncation_frac: fraction of entries in the Post table to truncate to
@@ -242,26 +287,26 @@ def build_dev_db(data_source: str,
         overwrite_existing: boolean indicating whether to overwrite a pre-existing database
     """
     # initialize database
-    if(os.path.isfile("dev_db.db")):
+    if(os.path.isfile(database_name)):
         if overwrite_existing:
-            warnings.warn("Found pre-existing dev_db.db, and overwrite_existing=True: removing file.")
-            os.remove("dev_db.db")
+            warnings.warn(f"Found pre-existing file {database_name}, and overwrite_existing=True: removing file.")
+            os.remove(database_name)
         else:
-            raise FileExistsError("Found pre-existing dev_db.db, and overwrite_existing=False:\n \
-                                   please move, remove, or rename existing dev database, or re-run with \
-                                   argument 'overwrite_existing=True'")
+            raise FileExistsError(f"Found pre-existing file {database_name}, and overwrite_existing=False:\n \
+                                  please select another name for new database; move, remove, or rename existing \
+                                  dev database; or re-run with argument 'overwrite_existing=True' to overwrite.")
 
     database_engine = database_engine.lower()
     match database_engine:
         case "sqlite":
-            db_conn = peewee.SqliteDatabase("dev_db.db")
+            db_conn = peewee.SqliteDatabase(database_name)
         case _:
             raise NotImplementedError(f"dev_database/build_dev_db: {database_engine} not supported, sorry!")
 
     database = DB(db_conn)
 
     # populate and process our data
-    database.populate_from_source(source=data_source, format=data_format)
+    database.populate_from_source(source=data_source, format=source_format)
     database.truncate(take_num=truncation_num, take_frac=truncation_frac, sampling=truncation_strat)
     database.clean()
 
