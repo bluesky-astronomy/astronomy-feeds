ddab874c380b731f0258b0a3ceaf537115a07cc4
Emily Hunt <emily.hunt.physics@gmail.com>
2025-05-26 21:11:14 +0200

    Add subscriptionstate & activitylog querying

--- scripts/sql/dev_db/populate_dev_db.sql
+++ scripts/sql/dev_db/populate_dev_db.sql
@@ -34,4 +34,21 @@ CREATE TABLE public.account (
 INSERT INTO public.account
 SELECT * from prod_public.account;
 
--- not taking SubscriptionState, ActivityLog, or NormalizedFeedStats for now
+-- SubscriptionState; take all for now
+DROP TABLE IF EXISTS public.subscriptionstate;
+CREATE TABLE public.subscriptionstate (
+    like prod_public.subscriptionstate including all
+);
+INSERT INTO public.subscriptionstate
+SELECT * from prod_public.subscriptionstate;
+
+-- ActivityLog; take all times Emily has viewed the feeds for now...
+-- TODO: replace with anonymizing the DID of users viewing the feed
+DROP TABLE IF EXISTS public.activitylog;
+CREATE TABLE public.activitylog (
+    like prod_public.activitylog including all
+);
+INSERT INTO public.activitylog
+SELECT * from prod_public.activitylog WHERE prod_public.activitylog.request_user_did = 'did:plc:jcoy7v3a2t4rcfdh6i4kza25' LIMIT 1000;
+
+-- not taking NormalizedFeedStats for now - maybe generate on the fly?
